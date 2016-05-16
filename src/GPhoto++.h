20ab807ea9f6350395241b57159b07aee800b55a
Marco Gulino <marco@gulinux.net>
2016-05-16 22:23:03 +0200

    Main include file

---
+++ src/GPhoto++.h
@@ -0,0 +1,36 @@
+/*
+ * libgphoto++ - modern c++ wrapper library for gphoto2
+ * Copyright (C) 2016 Marco Gulino <marco AT gulinux.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef LIBGPHOTO_CPP_GPHOTOCPP_MAIN_HEADER
+#define LIBGPHOTO_CPP_GPHOTOCPP_MAIN_HEADER
+
+#include "utils/logger.h"
+#include "utils/read_jpeg_image.h"
+#include "utils/read_raw_image.h"
+#include "backend/exceptions.h"
+#include "backend/gphoto_wrapper.h"
+#include "camera.h"
+#include "camera_control.h"
+#include "camera_filesystem.h"
+#include "camera_settings.h"
+#include "camerafile.h"
+#include "driver.h"
+#include "exposure.h"
+#include "gphotowidgetshooter.h"
+#include "serialshooter.h"
+
+#endif
